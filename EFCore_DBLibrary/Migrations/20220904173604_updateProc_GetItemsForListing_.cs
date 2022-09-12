e8acfac32a8e0d4ad10c42b0d8ffed0562857db2
Manuel Alfaro <manuel.alfaro1488@gmail.com>
2022-09-11 21:18:48 -0500

    Agregar archivos de proyecto.

---
+++ EFCore_DBLibrary/Migrations/20220904173604_updateProc_GetItemsForListing_.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using EFCore_DBLibrary.Migrations.Scripts;
+#nullable disable
+
+namespace EFCore_DBLibrary.Migrations
+{
+    public partial class updateProc_GetItemsForListing_ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlResource("EFCore_DBLibrary.Migrations.Scripts.Procedures.GetItemsForListing.GetItemsForListing.v1.sql");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlResource("EFCore_DBLibrary.Migrations.Scripts.Procedures.GetItemsForListing.GetItemsForListing.v0.sql");
+        }
+    }
+}
